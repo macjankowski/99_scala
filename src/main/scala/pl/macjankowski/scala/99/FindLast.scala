aaf74a956af818693ba6fa102610b46edcb20e8c
macjankowski <mac.jankowski@gmail.com>
2013-07-20 22:12:27 +0200

    deleted refactored files

--- src/main/scala/pl/macjankowski/scala/99/FindLast.scala
+++
@@ -1,9 +0,0 @@
-
-class FindLast(){
-	
- def last(list: List[Int]): Int = list match {
-	 case Nil => throw new java.util.NoSuchElementException("-1")
-	 case x :: Nil => return x
-	 case x :: xs => last(xs)
- }
-}
