33fc0a367a2f73b0e4c7ceda45884c860c7ca9f1
macjankowski <mac.jankowski@gmail.com>
2013-07-22 22:17:46 +0200

    corrected impl of last, corrected tests

--- src/main/scala/pl/macjankowski/scala/ninetynine/FindLast.scala
+++ src/main/scala/pl/macjankowski/scala/ninetynine/FindLast.scala
@@ -11,20 +11,19 @@ object FindLast {
     case x :: Nil => x
     case x :: xs => last(xs)
   }
-  
+
   @tailrec
   def lastG[T](list: List[T]): T = list match {
     case Nil => throw new java.util.NoSuchElementException("-1")
     case x :: Nil => x
     case x :: xs => lastG(xs)
   }
-  
+
   @tailrec
-  def lastT[T](t: Traversable[T]): T = t match {
-    case Nil => throw new java.util.NoSuchElementException("-1")
-    case x :: Nil =>  x
-    case x :: xs => lastT(xs)
+  def lastT[T](t: Traversable[T]): T = t.toSeq match {
+    case Seq() => throw new java.util.NoSuchElementException("-1")
+    case Seq(x) => x
+    case Seq(x, xs @ _*) => lastT(xs)
   }
-  
-  
+
 }
--- src/test/scala/pl/macjankowski/scala/ninetynine/TestFindLast.scala
+++ src/test/scala/pl/macjankowski/scala/ninetynine/TestFindLast.scala
@@ -2,45 +2,50 @@ package pl.macjankowski.scala.ninetynine
 
 import org.scalatest.FunSpec
 import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.prop.TableDrivenPropertyChecks
 
 /**
  * @author mac
  *
  */
-class TestFindLast extends FunSpec with ShouldMatchers {
+class TestFindLast extends FunSpec with ShouldMatchers with TableDrivenPropertyChecks {
+
+  val wrongParams = Table(
+    ("colName", "collection", "result"),
+    ("Empty list", Nil, -1),
+    ("Empty set", Set.empty, -1))
+
+  val intParams = Table(
+    ("colName", "collection", "result"),
+    ("List", List(1, 2, 3, 4, 5), 5),
+    ("List", List(3), 3),
+    ("Seq", Set(1, 2, 3, 4), 4))
+
+  val doubleParams = Table(
+    ("colName", "collection", "result"),
+    ("Seq", Seq(1.0, 4.3, 9.3), 9.3),
+    ("List", List(1.0, 4.3, 9.3), 9.3))
 
   describe("FindLast.last") {
-    it("should return last element of a list") {
-      FindLast.last(List(1, 2, 3, 4, 5)) should equal(5)
+
+    forAll(wrongParams) { (colName: String, col: Traversable[Nothing], res) =>
+      it(s"should throw exception for ${colName}") {
+        evaluating {
+          FindLast.lastT(col)
+        } should produce[NoSuchElementException]
+      }
     }
 
-    it("should throw exception for empty list") {
-      evaluating { FindLast.last(Nil) } should produce[NoSuchElementException]
+    forAll(intParams) { (colName: String,col: Traversable[Int], res) =>
+      it(s"should return $res for ${colName} with ints ${col.mkString(", ")}") {
+        FindLast.lastT(col) should equal(res)
+      }
     }
 
-    it("should return the only element of a list of size 1") {
-      FindLast.last(List(3)) should equal(3)
-    }
-  }
-
-  describe("FindLast generic version") {
-    it("should work for double") {
-      FindLast.lastG(List(1.0, 4.3, 9.3)) should equal(9.3)
-    }
-
-    it("should work for string") {
-      FindLast.lastG(List("a", "b", "c")) should equal("c")
-    }
-  }
-
-  describe("FindLast in Traversable") {
-    
-    it("should work for seq") {
-      FindLast.lastT(Seq(1.0, 4.3, 9.3)) should equal(9.3)
-    }
-    
-    it("should work for set") {
-      FindLast.lastT(Set(1.0, 4.3, 9.3)) should equal(9.3)
+    forAll(doubleParams) { (colName: String,col: Traversable[Double], res) =>
+      it(s"should return $res for ${colName} with doubles ${col.mkString(", ")}") {
+        FindLast.lastT(col) should equal(res)
+      }
     }
 
   }
